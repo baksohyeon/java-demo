0cfa6a6f31767b74b78a2ff434d0f232b91efac1
baksohyeon <bori12370@gmail.com>
2024-02-09 19:12:27 +0900

    feat: CORS 설정한 greetings API 추가

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
@@ -18,5 +20,13 @@ public class GreetingController {
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  }
+
+
+  @CrossOrigin(origins = "http://localhost:9000")
+  @GetMapping("greetings")
+  public Greetings greetings(@RequestParam(required = false, defaultValue = "World") String name) {
+    System.out.println("==== get greeting ====");
+    return new Greetings(counter.incrementAndGet(), String.format(template, name));
   }
 }

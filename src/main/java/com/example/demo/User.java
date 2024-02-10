125d297d93fa1e5ce73d3c4e2e541c553a7c4cff
baksohyeon <bori12370@gmail.com>
2024-02-10 15:48:02 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  private String email;
+
+}

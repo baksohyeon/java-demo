0c8cbce2a93ac2bb9e9f78578e1fc8b2e69f5a07
baksohyeon <bori12370@gmail.com>
2024-02-11 00:39:53 +0900

    feat: 질문 업데이트 하는 API 추가

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.model.Question;
 import com.example.demo.repository.QuestionRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,20 @@ public class QuestionController {
   public List<Question> getQuestionBySubjectLike(@RequestParam String subject) {
     return this.questionRepository.findBySubjectLike(String.format("%s%%", subject));
   }
+
+  @PutMapping(path = "/change")
+  public Question updateSubjectById(@RequestParam Integer id) {
+    Optional<Question> partialQuestion = this.questionRepository.findById(id);
+    if (partialQuestion.isPresent()) {
+      Question question = partialQuestion.get();
+      question.setSubject("수정된 제목!");
+      question.setContent("수정된 내용!");
+      this.questionRepository.save(question);
+      return question;
+    }
+    return null;
+  }
+
 
 
   public QuestionController(QuestionRepository questionRepository) {

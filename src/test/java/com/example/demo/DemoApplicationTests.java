97930a958155b93213adb72491807a8e4a5bdfbb
baksohyeon <bori12370@gmail.com>
2024-02-12 05:33:00 +0900

    시도 6차
    workflow 코드 하드코딩으로 수정함

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,13 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = {
-    "datasource.url=jdbc:mysql://localhost:3306/db_example",
-    "datasource.username=root",
-    "datasource.password=''",
-})
 
 @SpringBootTest
 class DemoApplicationTests {

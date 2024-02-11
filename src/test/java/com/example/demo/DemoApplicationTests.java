ad51744950b496779b3bfee50663401d6895271d
baksohyeon <bori12370@gmail.com>
2024-02-12 05:30:50 +0900

    시도 5차
    ㅠㅠ

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,13 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = {
+    "datasource.url=jdbc:mysql://localhost:3306/db_example",
+    "datasource.username=root",
+    "datasource.password=''",
+})
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -11,3 +18,5 @@ class DemoApplicationTests {
     }
 
 }
+
+
